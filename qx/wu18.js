b60a0d9fb97372a7344220718f06c87a0bc169d2
kingreevice <129922512+kingreevice@users.noreply.github.com>
2026-04-21 22:35:10 +0800

    Update
    Update

--- qx/wu18.js
+++ qx/wu18.js
@@ -1,14 +1,11 @@
 /*
 [rewrite_local]
-^http://wu18\.cn/cg/zhanghao/ALogOn\.php url script-response-body https://raw.githubusercontent.com/andyyds-song/own/refs/heads/main/qx/wu18.js
+^http://wu18\.cn/cg/zhanghao/ALogOn\.php url script-response-body https://raw.githubusercontent.com/andyyds-song/own/refs/heads/main/qx/wu18_response.js
 [mitm]
 hostname = wu18.cn
 */
 
-console.log("脚本已执行");
-
 let body = JSON.parse($response.body);
 body.data = "登录成功";
 body.time = "2026.12.13";
-
 $done({ body: JSON.stringify(body) });
