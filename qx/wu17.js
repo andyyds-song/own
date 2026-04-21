40e8e220576a903c0ebae1a77d48dbadc64d0f59
kingreevice <129922512+kingreevice@users.noreply.github.com>
2026-04-21 22:29:08 +0800

    Update
    Update

---
+++ qx/wu17.js
@@ -0,0 +1,8 @@
+/*
+[rewrite_local]
+^http://wu18\.cn/cg/zhanghao/ALogOn\.php url script-request-header https://raw.githubusercontent.com/andyyds-song/own/refs/heads/main/qx/wu18.js
+[mitm]
+hostname = wu18.cn
+*/
+
+$done({});
